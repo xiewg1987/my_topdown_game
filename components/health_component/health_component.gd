647d255a5141877ecf9587cdadf5a4244738e0b2
xieweiguang <xieweiguang11@163.com>
2026-01-27 13:38:32 +0800

    feat: 15.Health Component(健康组件) 单组件无需场景，添加时通过class_name在子节点中获取和添加普通节点一样

---
+++ components/health_component/health_component.gd
@@ -0,0 +1,35 @@
+class_name HealthComponent
+extends Node2D
+
+signal on_unit_damaged(amount: float) ## 攻击信号
+signal on_unit_healed(amount: float) ## 健康变化信号
+signal on_unit_dead ## 单位死亡信号
+
+var max_health: float ## 最大血量
+var current_health: float ## 当前血量
+
+
+## 初始化血量
+func init_health(value: float) -> void:
+	max_health = value
+	current_health = value
+
+## 受击函数
+func take_damage(value: float) -> void:
+	if current_health >= 0.0: # 建议类型统一
+		current_health -= value
+		on_unit_damaged.emit(value)
+		if current_health <= 0.0:
+			die()
+
+## 死亡
+func die() -> void:
+	current_health = 0.0
+	on_unit_dead.emit()
+
+
+## 健康
+func heal(value: float) -> void:
+	if current_health >= max_health: return
+	current_health = min(max_health, current_health + value)
+	on_unit_healed.emit(value)
