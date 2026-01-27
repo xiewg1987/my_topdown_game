d0b696df695afec9a63b1f92f309d015720b69e8
xieweiguang <xieweiguang11@163.com>
2026-01-27 10:16:53 +0800

    feat: 10.Save Settings(保存配置) 开发模式关闭音乐、音效、窗口模式

--- autoloads/global/global.gd
+++ autoloads/global/global.gd
@@ -1,7 +1,28 @@
 extends Node
 
+var save_setting_path := "user://settings.json"
+
+
 var settings: Dictionary = {
 	"music": true,
 	"sfx": true,
 	"fullscreen": false
 }
+
+
+## 保存配置
+func save_date() -> void:
+	var data =  settings.duplicate() # 对数据进行拷贝
+	var file = FileAccess.open(save_setting_path, FileAccess.WRITE) # 打开文件设置写入操作
+	file.store_string(JSON.stringify(data)) # 转成String保存
+	file.close() # 关闭文件
+
+
+## 读取文件
+func load_date() -> void:
+	if not FileAccess.file_exists(save_setting_path): return # 检查是否存在配置文件
+	var file = FileAccess.open(save_setting_path, FileAccess.READ) # 打开文件设置读取操作
+	var data = JSON.parse_string(file.get_as_text()) # 读取文件并转成JSON格式
+	file.close() # 关闭文件
+	settings = data # 获取输出保存至设置
+	
--- scenes/ui/main_menu/main_menu.gd
+++ scenes/ui/main_menu/main_menu.gd
@@ -9,7 +9,8 @@ const BusType: Dictionary = {
 const MAIN_TWEEN_TIME = 0.2 ## 主窗口补间时长
 const SETTINGS_TWEEN_TIME = 0.3 ## 设置窗口补间时长
 
-@export var click_stream: AudioStream 
+@export var music_stream: AudioStream
+@export var click_stream: AudioStream
 
 @onready var main_buttons: Control = %MainButtons ## 主窗口按钮组
 @onready var settings_buttons: Control = %SettingsButtons ## 设置按钮组
@@ -23,9 +24,9 @@ const SETTINGS_TWEEN_TIME = 0.3 ## 设置窗口补间时长
 @onready var window_button: TextureButton = %WindowButton ## 窗口按钮
 @onready var back_button: TextureButton = %BackButton ## 返回按钮
 
-@onready var music_label: Label = %MusicLabel
-@onready var sfx_label: Label = %SFXLabel
-@onready var window_label: Label = %WindowLabel
+@onready var music_label: Label = %MusicLabel ## 音乐label
+@onready var sfx_label: Label = %SFXLabel ## 音效label
+@onready var window_label: Label = %WindowLabel ## 窗口label
 
 
 var main_buttons_position: Vector2
@@ -39,30 +40,34 @@ var key_fsx = tr("KEY_SFX")
 var key_window = tr("KEY_WINDOW")
 
 func _ready() -> void:
-	## 国际化配置
+	# 国际化配置
 	TranslationServer.set_locale("zh")
 	
+	Global.load_date()
 	update_audio_bus(BusType.MUSIC, music_label, Global.settings.music)
 	update_audio_bus(BusType.SFX, sfx_label, Global.settings.sfx)
 	update_fullscreen(Global.settings.fullscreen)
 	
 	main_buttons_position = main_buttons.position
 	settings_buttons_position_x = settings_buttons.position.x
-	## 连接开始游戏信号
+	
+	# 连接开始游戏信号
 	play_button.pressed.connect(_on_play_button_pressed)
-	## 连接设置按钮按下信号
+	# 连接设置按钮按下信号
 	settings_button.pressed.connect(_on_settings_button_pressed)
-	## 连接退出按钮信号
+	# 连接退出按钮信号
 	quit_button.pressed.connect(_on_quit_button_pressed)
 	
-	## 连接音乐开关信号
+	# 连接音乐开关信号
 	music_button.pressed.connect(_on_music_button_pressed)
-	## 连接音效开关信号
+	# 连接音效开关信号
 	sfx_button.pressed.connect(_on_sfx_button_pressed)
-	## 连接窗口信号
+	# 连接窗口信号
 	window_button.pressed.connect(_on_window_button_pressed)
-	## 连接设置按钮组返回信号
+	# 连接设置按钮组返回信号
 	back_button.pressed.connect(_on_back_button_pressed)
+	# 播放背景音乐
+	MusicPlayer.play(music_stream)
 
 
 func update_audio_bus(bus_name: String, label: Label, is_on: bool) -> void:
@@ -92,20 +97,25 @@ func _on_settings_button_pressed() -> void:
 
 func _on_quit_button_pressed() -> void:
 	SFXPlayer.play(click_stream)
+	Global.save_date()
 	get_tree().quit()
 
 
 func _on_music_button_pressed() -> void:
 	SFXPlayer.play(click_stream)
+	Global.settings.music = not Global.settings.music
 	update_audio_bus(BusType.MUSIC, music_label, Global.settings.music)
+
 
 func _on_sfx_button_pressed() -> void:
 	SFXPlayer.play(click_stream)
+	Global.settings.sfx = not Global.settings.sfx
 	update_audio_bus(BusType.SFX, sfx_label, Global.settings.sfx)
 
 
 func _on_window_button_pressed() -> void:
 	SFXPlayer.play(click_stream)
+	Global.settings.fullscreen = not Global.settings.fullscreen
 	update_fullscreen(Global.settings.fullscreen)
 
 
@@ -115,4 +125,8 @@ func _on_back_button_pressed() -> void:
 	tween.tween_property(settings_buttons, "global_position:x", settings_buttons_position_x, SETTINGS_TWEEN_TIME)
 	tween.tween_interval(0.1)
 	tween.tween_property(main_buttons, "global_position:y", main_buttons_position.y, MAIN_TWEEN_TIME)
-	
+
+
+func _notification(what: int) -> void:
+	if what == NOTIFICATION_WM_CLOSE_REQUEST:
+		Global.save_date()
