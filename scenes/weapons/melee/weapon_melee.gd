b03f953d2dc876a373268b6878edff0adb5c4650
xiewg <xie0817@thundersoft.com>
2026-01-31 14:13:11 +0800

    feat: 37.Melee Attack(添加斧头场景)

--- scenes/weapons/melee/weapon_melee.gd
+++ scenes/weapons/melee/weapon_melee.gd
@@ -1,2 +1,28 @@
 class_name WeaponMelee
 extends Weapon
+
+
+var can_use: bool
+
+func _ready() -> void:
+	cooldown.wait_time = weapon_resource.cooldown
+
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("shoot"):
+		use_weapon()
+
+
+func use_weapon() -> void:
+	if not can_use: return
+	can_use = false
+	cooldown.start()
+	SFXPlayer.play(shoot_sound)
+	animation_player.play("melee/slash")
+	slash.global_rotation = pivot.global_rotation
+	slash.emitting = true
+
+
+func _on_cooldown_timeout() -> void:
+	can_use = true
+	animation_player.play("melee/idle")
--- scenes/weapons/weapon.gd
+++ scenes/weapons/weapon.gd
@@ -4,10 +4,17 @@ extends Node2D
 @export var weapon_resource: WeaponResource
 @export var shoot_sound: AudioStream
 
-@onready var sprite: Sprite2D = %Sprite2D
+# 通用属性
 @onready var pivot: Node2D = %Pivot
+@onready var sprite: Sprite2D = %Sprite2D
+
+# 远程属性
 @onready var fire_position: Marker2D = %FirePosition
 
+# 近战属性
+@onready var cooldown: Timer = %Cooldown
+@onready var slash: GPUParticles2D = %SlashParticle
+@onready var animation_player: AnimationPlayer = %AnimationPlayer
 
 func use_weapon() -> void:
 	pass
