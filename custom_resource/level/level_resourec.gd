1249726333ed009c631383f86141d50186456769
xiewg <xie0817@thundersoft.com>
2026-02-01 12:27:14 +0800

    feat: 44.Level Data(等级数据)

---
+++ custom_resource/level/level_resourec.gd
@@ -0,0 +1,12 @@
+class_name LevelResource
+extends Resource
+
+
+@export var num_sub_levels := 4 ## 子层级
+@export var num_rooms := 10 ## 房间数量
+@export var room_size := Vector2i(384, 384) ## 房间大小
+@export var room_scene: PackedScene ## 房间场景
+@export var min_enemies_per_room := 5 ## 房间最大敌人
+@export var max_enemies_per_room := 10 ## 房间最小敌人
+@export var enemy_scenes: Array[PackedScene] ## 敌人场景
+@export var store_resource: Array[LevelStoreResource] ## 商店资源
---
+++ custom_resource/level/level_store_resource.gd
@@ -0,0 +1,6 @@
+class_name LevelStoreResource
+extends Resource
+
+
+@export var item_resource: Resource
+@export var item_prob: float
