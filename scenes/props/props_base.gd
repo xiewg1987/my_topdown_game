a72ce3e70496a5b4f372fc360608e0846ab1616c
xieweiguang <xieweiguang11@163.com>
2026-02-03 12:31:37 +0800

    feat: 54.Create Props(创建道具)

---
+++ scenes/props/props_base.gd
@@ -0,0 +1,8 @@
+class_name Prop
+extends Area2D
+
+@onready var icon: Sprite2D = %Icon
+
+func _on_body_entered(_body: Node2D) -> void:
+	var range = randi_range(0, 1)
+	DampedOscillator.animate(icon, "scale", 250, 10, 17, 0.5 * range)
